290e26e3adbe6bf04269348d43d8bcd94b15ea50
Alex <antoine@etu.fr>
2021-12-16 11:25:54 +0100

    Nouvelle version du robot ricochet, manque le reset du chrono

--- grp-rose/scripts/move.py
+++ grp-rose/scripts/move.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
 
 #Import dependecies
-import math, rospy
+import math, rospy, random
 
 #Import msg
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 MIN_DISTANCE = 1
-
+ANGLE_DE_VUE = 150
+NB_SEQ_TOUR_COMPLET = 126
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
 
-
 def callback(data):
-    #rospy.loginfo(len(data.ranges))
-    min_range = min(data.ranges[round((len(data.ranges)/2)-100):round((len(data.ranges)/2)+100)])
-    #rospy.loginfo(min_range)
+    min_range = min(data.ranges[round((len(data.ranges)/2)-ANGLE_DE_VUE):round((len(data.ranges)/2)+ANGLE_DE_VUE)])
     if (min_range < MIN_DISTANCE):
         cmd.linear.x = 0.0
-        cmd.angular.z = 0.5
+        if data.header.seq%(2*NB_SEQ_TOUR_COMPLET) < NB_SEQ_TOUR_COMPLET:
+            cmd.angular.z = -0.5
+        else:
+            cmd.angular.z = 0.5
     else:
         cmd.linear.x = 1.0
         cmd.angular.z = 0.0
+     
+        
     commandPublisher.publish(cmd)
 
 
-sub =rospy.Subscriber("base_scan", LaserScan, callback)
+sub = rospy.Subscriber("base_scan", LaserScan, callback)
 commandPublisher = rospy.Publisher( '/cmd_vel', Twist, queue_size=10)
 cmd = Twist()
 
