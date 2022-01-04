9080188701ba9ee228abaf65080007e215c4d953
Alex <alexnclas@gmail.com>
2022-01-04 23:25:25 +0100

    Segmentation HSV

--- tp_vision/gestion_souris.py
+++ tp_vision/gestion_souris.py
@@ -2,22 +2,17 @@ import cv2
 import numpy as np
 
 # connect to a sensor (0: webcam)
-#cap=cv2.VideoCapture('/home/antoine/1920px-Artemis_III.jpg')
-cap=cv2.VideoCapture(-1)
+cap=cv2.VideoCapture(0)
 
 # capture an image
 ret, frame=cap.read()
 
 # Select ROI
-# r = cv2.selectROI(frame)
+r = cv2.selectROI(frame)
 
 # Crop image
-#imCrop = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+imCrop = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
 
 # Display cropped image
-# cv2.imshow("Image", imCrop)
-
-cv2.imshow("image", frame)
-#cv2.imshow("/home/antoine/1920px-Artemis_III.jpg", imCrop)
-
+cv2.imshow("Image", imCrop)
 cv2.waitKey(0)
