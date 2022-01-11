40556d212e05a0c7af181332e7f7a7253f7be33a
Alex <alexnclas@gmail.com>
2022-01-11 21:49:55 +0100

    Segmentation couleur challenge2

--- grp-rose/scripts/segmentation_couleur.py
+++ grp-rose/scripts/segmentation_couleur.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-#Constantes
+#Constantes seuils
 lowBadge=np.array([0, 35, 100]) 
 hiBadge=np.array([6, 255, 255])
 
@@ -16,35 +16,32 @@ while cap.isOpened():
     
     #Recup Frame
     ret, frame=cap.read()
+    #Transformation en hsv
     img_hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #Mask Badge 
+    #Mask Badge (Double seuillage puis amelioration)
     maskBadge=cv2.inRange(img_hsv, lowBadge, hiBadge)
     maskBadge=cv2.erode(maskBadge, None, iterations=1)
     maskBadge=cv2.dilate(maskBadge, None, iterations=1)
 
-
-    #Mask Ruban
+    #Mask Ruban (Double seuillage puis amelioration)
     maskRuban=cv2.inRange(img_hsv, lowRuban, hiRuban)
     maskRuban=cv2.erode(maskRuban, None, iterations=1)
     maskRuban=cv2.dilate(maskRuban, None, iterations=1)
 
-    #Add des mask
+    #Combinaison des masks
 
     mask = cv2.add(maskBadge, maskRuban)
 
+    #Extraction des zones d'interets
     img_result=cv2.bitwise_and(frame, frame, mask= mask)
 
     #FILLING HOLE ET ELIMINATION DES AUTRES FORMES ???
 
-
-
     #Affichage
-    cv2.imshow('color image',frame) 
+    cv2.imshow('Camera',frame) 
     cv2.waitKey(1)  
     cv2.imshow('Result',img_result)  
-    cv2.waitKey(1)
-    cv2.imshow('Camera', frame)
     cv2.waitKey(1)
 cap.release()
 cv2.destroyAllWindows()
