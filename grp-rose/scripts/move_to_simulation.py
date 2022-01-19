429262ab4e0395dd35abe265c9118bd6f112ca2c
Antoine <antoine.delaroche28130@gmail.com>
2022-01-19 10:19:45 +0100

    set constants

--- grp-rose/scripts/move_to_simulation.py
+++ grp-rose/scripts/move_to_simulation.py
@@ -9,6 +9,28 @@ from math import atan2, sin, cos, pow, sqrt
 from geometry_msgs.msg import Twist, PoseStamped
 from nav_msgs.msg import Odometry
 
+# CONSTANTES
+
+COEFF_VITESSE_TURN = 0.2
+
+DISTANCE_1 = 3.0
+VITESSE_1 = 1.0
+
+DISTANCE_2 = 2.0
+VITESSE_2 = 0.8
+
+DISTANCE_3 = 1.0
+VITESSE_3 = 0.6
+
+DISTANCE_4 = 0.5
+VITESSE_4 = 0.4
+
+DISTANCE_5 = 0.3
+VITESSE_5 = 0.2
+
+DISTANCE_6 = 0.2
+VITESSE_6 = 0.1
+
 class MoveRobotTo:
 
     def __init__(self):
@@ -57,25 +79,27 @@ class MoveRobotTo:
         if abs(angle_to_goal - self.theta) < 0.1:
             self.move_forward = True
 
-        self.cmd.angular.z = 0.2 * turn
+        self.cmd.angular.z = COEFF_VITESSE_TURN * turn
 
         if self.move_forward == True:
             # speed reduction when dist decrease
-            if dist > 3.0:
-                dist = 0.7
-            elif dist > 2.0:
-                dist = 0.5
-            elif dist > 1.0:
-                dist = 0.3
-            elif dist > 0.5:
-                dist = 0.2
-            elif dist > 0.2:
-                dist = 0.1
-            elif dist > 0.05:
-                dist = 0.05
+            if dist > DISTANCE_1:
+                speed = VITESSE_1
+            elif dist > DISTANCE_2:
+                speed = VITESSE_2
+            elif dist > DISTANCE_3:
+                speed = VITESSE_3
+            elif dist > DISTANCE_4:
+                speed = VITESSE_4
+            elif dist > DISTANCE_5:
+                speed = VITESSE_5
+            elif dist > DISTANCE_6:
+                speed = VITESSE_6
             else:
-                dist = 0.0
+                speed = 0.0
                 self.cmd.angular.z = 0.0
+
+            self.cmd.linear.x = speed
 
             self.cmd.linear.x = dist
 
