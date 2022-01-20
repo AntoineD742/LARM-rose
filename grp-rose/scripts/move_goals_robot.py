981e7cea4469d086de9db944e3398f072461c92a
Antoine <antoine.delaroche28130@gmail.com>
2022-01-20 16:08:16 +0100

    suppression fichiers inutilisés et modif move_goals

--- grp-rose/scripts/move_goals_robot.py
+++ grp-rose/scripts/move_goals_robot.py
@@ -13,29 +13,15 @@ from sensor_msgs.msg import LaserScan, Image
 
 from cv_bridge import CvBridge, CvBridgeError
 
-#CONSTANTES / PARAMETRES
+# CONSTANTES MODE AUTONOME
 VITESSE_LINEAIRE_ROBOT_MIN = 0.1
 VITESSE_LINEAIRE_ROBOT_MAX = 0.3
 VITESSE_ANGULAIRE_ROBOT_MIN = 0.5
 VITESSE_ANGULAIRE_ROBOT_MAX = 1
 
+# CONSTANTES MODE GOAL
 
-MIN_DISTANCE_X = 0.25 #Distance pour laquelle le robot considère l'obstacle comme à éviter (Sur l'axe X)
-MIN_DISTANCE_Y = 0.2  #Distance pour laquelle le robot considère l'obstacle comme à éviter (Sur l'axe Y)
-AVOID_DISTANCE_X = 1.5 #Distance pour laquelle le robot considère l'obstacle comme lointain (Sur l'axe X)
-AVOID_DISTANCE_Y = 0.8 #Distance pour laquelle le robot considère l'obstacle comme lointain (Sur l'axe Y)
-# AVOID_DISTANCE_X = 1 #Distance pour laquelle le robot considère l'obstacle comme lointain (Sur l'axe X)
-# AVOID_DISTANCE_Y = 0.8 #Distance pour laquelle le robot considère l'obstacle comme lointain (Sur l'axe Y)
-
-OBSTACLE_PIXEL_SIZE = 2000 #Nombre de pixel à partir duquel on considère qu'un obstacle est proche de la caméra
-
-#OLD
-BEHIND_THE_ROBOT = 50 #On set la distance des objets derrière le laser à un nombre infiniment grand
-
-
-# CONSTANTES
-
-COEFF_VITESSE_TURN = 0.2
+COEFF_VITESSE_TURN = 0.5
 
 DISTANCE_1 = 3.0
 VITESSE_1 = 0.7
@@ -49,33 +35,39 @@ VITESSE_3 = 0.4
 DISTANCE_4 = 0.5
 VITESSE_4 = 0.2
 
-DISTANCE_5 = 0.3
+DISTANCE_5 = 0.4
 VITESSE_5 = 0.1
 
-DISTANCE_6 = 0.2
+DISTANCE_6 = 0.3
 VITESSE_6 = 0.05
+
+# CONSTANTES CONCERNANT L'EVITEMENT D'OBSTACLES
+
+MIN_DISTANCE_X = 0.25       #Distance pour laquelle le robot considère l'obstacle comme à éviter (Sur l'axe X)
+MIN_DISTANCE_Y = 0.2        #Distance pour laquelle le robot considère l'obstacle comme à éviter (Sur l'axe Y)
+AVOID_DISTANCE_X = 1.5      #Distance pour laquelle le robot considère l'obstacle comme lointain (Sur l'axe X)
+AVOID_DISTANCE_Y = 0.8      #Distance pour laquelle le robot considère l'obstacle comme lointain (Sur l'axe Y)
+
+OBSTACLE_PIXEL_SIZE = 2000  #Nombre de pixel à partir duquel on considère qu'un obstacle est proche de la caméra
+
+BEHIND_THE_ROBOT = 50       #On set la distance des objets derrière le laser à un nombre infiniment grand
 
 
 class RobotMouvement:
 
     def __init__(self):
         self.cmd = Twist()
-
         self.bridge = CvBridge()            #Conversion Images OpenCV-ROS
-        self.depth_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.callbackDepth)  #Recuperation image de profondeur (Alignée)
-        self.depth_map = None   #Frame Color    
+        self.depth_map = None               #Frame Color    
         self.depth_obstacle_ahead = False
-        
         self.generate_new_direction = False
-
+        self.autonome_mode = True
         self.goal = None
         self.move_forward = False
-
         self.x = None
         self.y = None
         self.theta = None
         self.dist = None
-
         self.current_forward_speed = 0
         self.current_angular_speed = 0
 
@@ -83,28 +75,26 @@ class RobotMouvement:
         self.commandPublisher = rospy.Publisher("/cmd_vel_mux/input/navi", Twist, queue_size = 10)   
         # Subscriber
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.update_goal)
-        rospy.Subscriber("/scan", LaserScan, self.callbackLaser)
         rospy.Subscriber('/odom', Odometry, self.update_pose)
-        # Timer
-        #rospy.Timer(rospy.Duration(0.1), self.move_command, oneshot=False)
+        rospy.Subscriber("/scan", LaserScan, self.callbackLaser)
+        rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.callbackDepth)
 
     def update_goal(self, goalData):
-        # Record goal pose given in goalData in odom frame
+        # Récupération des coordonnées du goal dans la frame odom
         self.goal = goalData
         self.move_forward = False
+        self.autonome_mode = False
+        self.current_forward_speed = 0.0
 
     def update_pose(self, goalPose):
-        # Record robot pose given in goalPose in odom frame
+        # Récupération de la pose du robot dans la frame odom
         self.x = goalPose.pose.pose.position.x
         self.y = goalPose.pose.pose.position.y
         rot_q = goalPose.pose.pose.orientation
         (roll, pitch, self.theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-
     
-    
-    
-    
-    def callbackLaser(self, data):    #Mouvement du robot selon la data reçu par le laser
+    def callbackLaser(self, data):
+        # Mouvement du robot selon la data reçu par le laser
         order = self.decisionMouvement(data)
         self.setFwdSpeed(order)
         self.setAngulaireSpeed(order)
@@ -114,7 +104,7 @@ class RobotMouvement:
     
     def callbackDepth(self,data):
         try:
-            #Recuperation image de profondeur (Alignée), Conveersion au format OpenCV
+            # Récupération image de profondeur (Alignée), conversion au format OpenCV
             self.depth_map = self.bridge.imgmsg_to_cv2(data, "passthrough")
             self.depth_map = cv2.convertScaleAbs(self.depth_map, alpha=0.1)
             self.depth_map = cv2.inRange(self.depth_map, 1 , 40)
@@ -128,7 +118,6 @@ class RobotMouvement:
                 self.depth_obstacle_ahead = True
             else:
                 self.depth_obstacle_ahead = False
-            #print(nbrPixelsDetectes)
 
         except CvBridgeError as e:
             print(e)
@@ -136,17 +125,18 @@ class RobotMouvement:
     
     
     def setFwdSpeed(self, order): 
-        #0 DEMI TOUR
-        #1 AVANCER TOUT DROIT (VITESSE MAX)
-        #2 OBJET LOINTAIN SUR LA GAUCHE
-        #3 OBJET LOINTAIN SUR LA DROITE
-        #4 OBJET PROCHE SUR LA GAUCHE
-        #5 OBJET PROCHE SUR LA DROITE
         if order == 0:
             #self.current_forward_speed = - VITESSE_LINEAIRE_ROBOT_MIN
             self.current_forward_speed = 0
-        elif order == 1:  
-            self.move_command_linear()
+        elif order == 1:
+            # On vérifie qu'on est en mode autonome quand l'ordre est 1
+            if self.autonome_mode:
+                if(self.current_forward_speed < VITESSE_LINEAIRE_ROBOT_MAX):
+                    self.current_forward_speed += 0.05
+                else:
+                    self.current_forward_speed = VITESSE_LINEAIRE_ROBOT_MAX
+            else:    
+                self.move_command_linear()
         elif order == 2 or order == 3:
             if(self.current_forward_speed > VITESSE_LINEAIRE_ROBOT_MIN):
                 self.current_forward_speed -= 0.01
@@ -157,24 +147,20 @@ class RobotMouvement:
         #print("Forward speed: " + str(self.current_forward_speed))
     
     def setAngulaireSpeed(self, order):
-        #0 DEMI TOUR
-        #1 AVANCER TOUT DROIT (VITESSE MAX)
-        #2 OBJET LOINTAIN SUR LA GAUCHE
-        #3 OBJET LOINTAIN SUR LA DROITE
-        #4 OBJET PROCHE SUR LA GAUCHE
-        #5 OBJET PROCHE SUR LA DROITE
         if order == 0:
             if self.generate_new_direction:
                 if random.random() < 0.5:
                     self.current_angular_speed = +VITESSE_ANGULAIRE_ROBOT_MAX
                 else:
                     self.current_angular_speed = -VITESSE_ANGULAIRE_ROBOT_MAX
-                self.generate_new_direction = False
-
-            
+                self.generate_new_direction = False  
         elif order == 1:
-            self.move_command_angular()
-            self.generate_new_direction = True #Prochaine fois que le robot devra faire demi-tour il generera aleatoirement une direction
+            # On vérifie qu'on est en mode autonome quand l'ordre est 1
+            if self.autonome_mode:
+                self.current_angular_speed = 0
+            else:
+                self.move_command_angular()
+            self.generate_new_direction = True # Prochaine fois que le robot devra faire demi-tour il generera aleatoirement une direction
         elif order == 2:
             self.current_angular_speed = -VITESSE_ANGULAIRE_ROBOT_MIN
         elif order == 3:
@@ -183,14 +169,13 @@ class RobotMouvement:
             self.current_angular_speed = -VITESSE_ANGULAIRE_ROBOT_MAX
         elif order == 5:
             self.current_angular_speed = +VITESSE_ANGULAIRE_ROBOT_MAX
-        #else:
-        #print("Angular speed: " + str(self.current_angular_speed))
         
 
 
     
 
-    def decisionMouvement(self,data):    #Calcul des distances entre le robot et l'obstacle le plus proche, prise d'une décision permettant d'éviter l'obstacles
+    def decisionMouvement(self,data):
+        # Calcul des distances entre le robot et l'obstacle le plus proche, prise d'une décision permettant d'éviter l'obstacles
         #0 DEMI TOUR
         #1 AVANCER TOUT DROIT (VITESSE MAX)
         #2 OBJET LOINTAIN SUR LA GAUCHE
@@ -236,16 +221,13 @@ class RobotMouvement:
 
 
     def move_command_linear(self, event=None):
-        # If there is no goal, do nothing
-        if self.goal is None:
-            return False
 
-        inc_x = self.goal.pose.position.x - self.x      # distance robot to goal in x
-        inc_y = self.goal.pose.position.y - self.y      # distance robot to goal in y
-        self.dist = sqrt(pow(inc_x, 2) + pow(inc_y, 2))      # calculate distance
+        inc_x = self.goal.pose.position.x - self.x       # distance robot-goal en x
+        inc_y = self.goal.pose.position.y - self.y       # distance robot-goal en y
+        self.dist = sqrt(pow(inc_x, 2) + pow(inc_y, 2))  # calcul distance
 
         if self.move_forward == True:
-            # speed reduction when dist decrease
+            # Plus la distance diminue plus la vitesse diminue
             if self.dist > DISTANCE_1:
                 self.current_forward_speed = VITESSE_1
             elif self.dist > DISTANCE_2:
@@ -260,33 +242,27 @@ class RobotMouvement:
                 self.current_forward_speed = VITESSE_6
             else:
                 self.current_forward_speed = 0.0
-                self.current_angular_speed = 0.0
 
-        return True
+
 
     def move_command_angular(self, event=None):
-        # If there is no goal, do nothing
-        if self.goal is None:
-            return False
 
-        inc_x = self.goal.pose.position.x - self.x      # distance robot to goal in x
-        inc_y = self.goal.pose.position.y - self.y      # distance robot to goal in y
-        angle_to_goal = atan2(inc_y, inc_x)             # calculate angle through distance from robot to goal in x and y
+        inc_x = self.goal.pose.position.x - self.x      # distance robot-goal en x
+        inc_y = self.goal.pose.position.y - self.y      # distance robot-goal en y
+        angle_to_goal = atan2(inc_y, inc_x)             # calcul de l'angle pour atteindre le goal
 
-        # find out which turndirection is better
-        # the bigger the angle, the bigger turn, - when clockwise
+        # On cherche la bonne direction pour tourner
+        # Plus l'angle est grand, plus la vitesse angulaire est grande
         turn = atan2(sin(angle_to_goal - self.theta), cos(angle_to_goal - self.theta))
 
         if abs(angle_to_goal - self.theta) < 0.1:
+            # Une fois que le robot est aligné avec le goal, le robot peut avancer
             self.move_forward = True
 
         if self.dist > DISTANCE_6:
             self.current_angular_speed = COEFF_VITESSE_TURN * turn
         else:
             self.current_angular_speed = 0.0
-
-        return True
-
 
 
 def main():
